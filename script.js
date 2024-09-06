e9d9e2ac6408b889e68d8131043baecedd896b47
vipulattri <133794719+vipulattri@users.noreply.github.com>
2024-09-06 22:44:15 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,42 @@
+function cursoreffect() {
+    var page1Content = document.querySelector("#page1-content")
+var cursor = document.querySelector("#cursor")
+
+page1Content.addEventListener("mousemove",function(dets){
+   gsap.to(cursor,{
+    x:dets.x,
+    y:dets.y
+   })
+
+})
+page1Content.addEventListener("mouseenter",function(){
+    gsap.to(cursor,{
+        scale:1,
+        opacity:1
+    })
+})
+page1Content.addEventListener("mouseleave",function(){
+  gsap.to(cursor,{
+    scale:0,
+    opacity:0
+  })  
+})
+
+}
+cursoreffect();
+
+function page2Animation() {
+    gsap.from(".elem h1",{
+        y:120,
+        stagger:0.2,
+        scrollTrigger: {
+            trigger:"#page2",
+            scroller:"body",
+            start:"top 47%",
+            end: "top 46%",
+            markers:true,
+            scrub:2
+        }
+    })
+}
+page2Animation();
